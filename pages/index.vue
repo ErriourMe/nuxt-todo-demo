dca623af3d49f3e676b11dfd220a82fc749ae9a1
Erriour <vl.err@yandex.ru>
2020-12-30 22:07:28 +0300

    Get data, remove items

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row py-3">
       <div class="col-12">
-        <div class="form-group">
+        <div class="btn-group w-100">
           <input v-model="form.name" type="text" class="form-control" />
-          <button @click="send()">Создать</button>
+          <button class="btn btn-danger" @click="send()">Создать</button>
         </div>
       </div>
     </div>
 
     <div class="row">
-      <div v-for="(todo, i) in todos" :key="`todo-${i}`" class="col-12">
-        <div class="container">
-          <div class="row">
-            <div class="col-12 col-md-6">
-              {{ todo.name }}
-            </div>
-            <div class="col-12 col-md-6 d-flex justify-content-end">
-              <button type="button" class="btn btn-primary" @click="edit(i)">
-                Редактировать
-              </button>
-              <button type="button" class="btn btn-danger" @click="remove(i)">
-                Удалить
-              </button>
-            </div>
+      <div v-for="(todo, i) in todos" :key="`todo-${i}`" class="col-12 mt-2">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            {{ todo.name }}
+          </div>
+          <div class="col-12 col-md-6 d-flex justify-content-end">
+            <button type="button" class="btn btn-primary" @click="edit(i)">
+              Редактировать
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger ml-2"
+              @click="remove(i)"
+            >
+              Удалить
+            </button>
           </div>
         </div>
       </div>
@@ -44,16 +46,20 @@ export default {
   },
   mounted() {
     this.$axios.get('http://starter-pack.io/api/tasks').then((res) => {
-      this.todos.push(this.form)
+      this.todos = res.data.data
     })
   },
   methods: {
     edit(i) {},
-    remove(i) {},
+    remove(i) {
+      this.todos.splice(i, 1)
+    },
     send() {
-      this.$axios.post('http://starter-pack.io/api/tasks').then((res) => {
-        this.todos = res.data
-      })
+      this.$axios
+        .post('http://starter-pack.io/api/tasks', this.form)
+        .then((res) => {
+          this.todos.push(this.form)
+        })
     },
   },
 }
